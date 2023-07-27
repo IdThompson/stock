ae9900722509c76e00c444194c528fdc06843c5e
Idowu Thompson <idowu.thompson@decagon.dev>
2023-07-27 06:48:55 +0100

    stock- application started

---
+++ src/main/java/com/activEgde/stock_Application/model/Stock.java
@@ -0,0 +1,30 @@
+package com.activEgde.stock_Application.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "stocks")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal currentPrice;
+    @Column(name = "created_date")
+    @CreationTimestamp
+    private Timestamp createDate;
+    @Column(name = "lastUpdated_date")
+    @UpdateTimestamp
+    private Timestamp lastUpdate;
+}
